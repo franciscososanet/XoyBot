3ed1c91fe925cd6d77c45dbf22c73aed198bb126
franciscososanet <franchovd@gmail.com>
2023-08-14 21:43:39 -0300

    Ahora saluda al username en guildmemberadd

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -5,10 +5,13 @@ module.exports = async (client, discord, member) => {
         const bienvenidaChannel = client.channels.cache.get("1119677625519067177");
         const reglasChannel = client.channels.cache.get("1134751084682948618");
         
+        // Comprueba si el miembro tiene un apodo. Si no lo tiene, usa el nombre de usuario.
+        const displayName = member.nickname ? member.nickname : member.user.username;
+
         const msg = new discord.MessageEmbed()
-            .setTitle(`✨ ¡Bienvenido, ${member.user.username}! ✨`)
+            .setTitle(`✨ ¡Bienvenido, ${displayName}! ✨`)
             .setColor("#d41393")
-            .setDescription(`🎮 ¡Holis! Estoy emocionada de tenerte acá ♥.  Este es el lugar perfecto para hacer amigos y jugar juntos.\n\n¡Espero que te diviertas !\n\nRecuerda revisar las ${reglasChannel}.\n¡Nos vemos en el juego!`)
+            .setDescription(`🎮 ¡Holis! Estoy emocionada de tenerte acá ♥. Este es el lugar perfecto para hacer amigos y jugar juntos.\n\n¡Espero que te diviertas!\n\nRecuerda revisar las ${reglasChannel}.\n¡Nos vemos en el juego!`)
             .setTimestamp();
 
         bienvenidaChannel.send({ embeds: [msg] });
